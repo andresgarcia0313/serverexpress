e113284435b0f6fb4b4eff35c3001eb56a394f24
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2024-09-24 19:24:46 -0500

    se mejora el código de base datos

--- database.js
+++ database.js
@@ -1,13 +1,10 @@
 const { MongoClient } = require("mongodb"); // Importa MongoClient de la librería nativa de MongoDB
 let db; // Variable para almacenar la conexión de la base de datos
-let url =
-  "mongodb+srv://andresgarcia0313:iHunvQv0IzNCXwhh@mycluster.ldxus.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster";
+let url = process.env.MONGODB_URL;
 async function connectDB() {
   try {
-    if (db) return db; // Si ya estamos conectados, devuelve la conexión actual
-    const client = await MongoClient.connect(url); // Conecta al servidor de MongoDB con la URL de conexión
-    console.log("Conectado a la base de datos"); // Mensaje de confirmación
-    db = client.db("contactosDB"); // Selecciona la base de datos
+    if (db) return db; // Si la conexión ya está establecida, retorna la conexión
+    db = (await MongoClient.connect(url)).db("contactosDB"); // Conecta y selecciona la base de datos
     return db;
   } catch (e) {
     console.error("Error conectando a la base de datos:", e);
